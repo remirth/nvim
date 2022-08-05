a50f0fa2000556a71431fbd2cf9ccb122fe16188
exsjabe <jakob.bergstrom@explizit.se>
2022-08-05 09:44:22 +0200

    More remaps

--- lua/exsjabe/remap.lua
+++ lua/exsjabe/remap.lua
@@ -5,7 +5,10 @@ local nnoremap = require("exsjabe.keymap").nnoremap
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<leader>pw", "<cmd>w<CR>")
 nnoremap("<leader>pf", "<cmd>NvimTreeFocus<CR>")
-nnoremap("<leader>pt", "<cmd>terminal<CR>")
+nnoremap("<leader>pp", "<cmd>terminal<CR>")
 inoremap("jj", "<esc>")
 
 
+nnoremap("<leader>tf", "<cmd>NvimTreeFocus<CR>")
+nnoremap("<leader>tr", "<cmd>NvimTreeRefresh<CR>")
+nnoremap("<leader>tc", "<cmd>NvimTreeCollapse<CR>")
