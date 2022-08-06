1e106abe48f99c96b947a004b83213be006c318d
exsjabe <jakob.bergstrom@explizit.se>
2022-08-06 11:18:01 +0200

    Removed unused language server

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -1,14 +1,14 @@
 -- Setup lspconfig.
--- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-servers = {'pyright', 'tsserver', 'angularls',
+local servers = {'pyright', 'tsserver', 'angularls',
 'vls', 'gopls', 'html',
-'cssls', 'eslint', 'powershell_es',
+'cssls', 'powershell_es',
 }
 for _, server in pairs(servers) do
     require('lspconfig')[server].setup {
       capabilities = capabilities,
-      on_attach = function(client) 
+      on_attach = function(client)
         client.resolved_capabilities.document_formatting = false
         client.resolved_capabilities.document_range_formatting = false
       end,
