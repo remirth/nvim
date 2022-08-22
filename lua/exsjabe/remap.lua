b904dc859a0d2b823daa0aae4af16edcc48c3a24
Jakob Bergström <jakob.bergstrom@explizit.se>
2022-08-22 14:59:16 +0200

    Added save overwrite

--- lua/exsjabe/remap.lua
+++ lua/exsjabe/remap.lua
@@ -3,7 +3,7 @@ local inoremap = require("exsjabe.keymap").inoremap
 local nnoremap = require("exsjabe.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
-nnoremap("<leader>pw", "<cmd>w<CR>")
+nnoremap("<leader>pw", "<cmd>w!<CR>")
 nnoremap("<leader>pf", "<cmd>Format<CR>")
 nnoremap("<leader>pp", "<cmd>terminal<CR>")
 inoremap("jj", "<esc>")
