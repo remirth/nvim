469d2baca6c2b492ad419bc8434f456d6765c8d7
Jakob Bergström <jakob.bergstrom@explizit.se>
2022-09-05 14:26:18 +0200

    Fixed lsp for vue

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -2,7 +2,7 @@
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 local servers = { 'pyright', 'tsserver',
-'vls', 'gopls', 'html', 'rust_analyzer',
+'vuels', 'gopls', 'html', 'rust_analyzer',
 'cssls', 'powershell_es', 'dockerls', 'svelte', 'tailwindcss'
 }
 for _, server in pairs(servers) do
