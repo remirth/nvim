abc1f3bdb11cc74824cdf2b6596726b665bce192
Exsjabe <jakob.bergstrom@explizit.se>
2023-03-31 08:46:59 +0200

    Updated remaps

--- lua/exsjabe/remap.lua
+++ lua/exsjabe/remap.lua
@@ -1,12 +1,8 @@
-local nnoremap = require("exsjabe.keymap").nnoremap
-local inoremap = require("exsjabe.keymap").inoremap
-local vnoremap = require("exsjabe.keymap").vnoremap
-
 vim.keymap.set("n", "l", "/")
 vim.keymap.set("v", "<C-f>", "=")
 vim.keymap.set("n", "<C-q>", "<C-6>")
-
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -48,9 +44,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
