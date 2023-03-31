c47d650399e1763d5ef5cc2d6e79fcfb7ebc10ae
Exsjabe <jakob.bergstrom@explizit.se>
2023-03-31 11:01:43 +0200

    Setup copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,3 +1,8 @@
 if (not vim.g.vscode) then
     vim.cmd.Copilot("setup")
+    vim.cmd.Copilot("enable")
+
+    vim.g.copilot_no_tab_map = false;
+    vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    
 end
