f0c7a7517241c3175546430b8fddbc79de18e865
Exsjabe <jakob.bergstrom@explizit.se>
2023-04-13 09:55:47 +0200

    changed bg

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,8 +3,8 @@ if (not vim.g.vscode) then
         color = color or "rose-pine"
         vim.cmd.colorscheme(color)
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 
     ColorMyPencils()
