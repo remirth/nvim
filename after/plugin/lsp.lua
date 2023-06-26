88b9c170999649104f05b85125abaad5d79446a3
Exsjabe <jakob.bergstrom@explizit.se>
2023-06-26 12:18:18 +0200

    Enabled copilot for markdown files

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,118 +2,118 @@ local lsp = require("lsp-zero")
 
 lsp.preset("recommended")
 lsp.ensure_installed({
-    "tsserver",
-    "eslint",
-    "rust_analyzer",
+  "tsserver",
+  "eslint",
+  "rust_analyzer",
 })
 
 
 
 
 local has_words_before = function()
-    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
 end
 
 local cmp = require("cmp")
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({
-        -- this is the important line
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-    }),
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-        if cmp.visible() and has_words_before() then
-            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-        else
-            fallback()
-        end
-    end),
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-Space>'] = cmp.mapping.complete(),
+  ["<CR>"] = cmp.mapping.confirm({
+    -- this is the important line
+    behavior = cmp.ConfirmBehavior.Replace,
+    select = false,
+  }),
+  ["<Tab>"] = vim.schedule_wrap(function(fallback)
+    if cmp.visible() and has_words_before() then
+      cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    else
+      fallback()
+    end
+  end),
 })
 
 lsp.setup_nvim_cmp({
-    mapping = cmp_mappings,
-    sources = {
-        -- Copilot Source
-        { name = "copilot",  group_index = 2 },
-        -- Other Sources
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "path",     group_index = 2 },
-        { name = "luasnip",  group_index = 2 },
-    },
+  mapping = cmp_mappings,
+  sources = {
+    -- Copilot Source
+    { name = "copilot",  group_index = 2 },
+    -- Other Sources
+    { name = "nvim_lsp", group_index = 2 },
+    { name = "path",     group_index = 2 },
+    { name = "luasnip",  group_index = 2 },
+  },
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
+  suggest_lsp_servers = false,
 })
 
 local function disallow_format(servers)
-    return function(client)
-        local contains = vim.tbl_contains(servers, client.name) == false;
-        return contains;
-    end
+  return function(client)
+    local contains = vim.tbl_contains(servers, client.name) == false;
+    return contains;
+  end
 end
 
 local group = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 local event = "BufWritePre" -- or "BufWritePost"
 local async = event == "BufWritePost"
 lsp.on_attach(function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        local allow_format = disallow_format({ "tsserver", "eslint" })
-        vim.keymap.set("n", "<C-f>", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), filter = allow_format, timeout_ms = 10000 })
-        end, { buffer = bufnr, desc = "[lsp] format" })
-        -- format on save
-        vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
-        vim.api.nvim_create_autocmd(event, {
-            buffer = bufnr,
-            group = group,
-            callback = function()
-                vim.lsp.buf.format({
-                    bufnr = bufnr,
-                    async = async,
-                    filter = allow_format,
-                    timeout_ms = 10000
-                })
-            end,
-            desc = "[lsp] format on save",
-        })
-    end
-
-    if client.supports_method("textDocument/rangeFormatting") then
-        vim.keymap.set("x", "<C-f>", function()
-            vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), filter = allow_format, timeout_ms = 10000 })
-        end, { buffer = bufnr, desc = "[lsp] format" })
-    end
-
-    local opts = { buffer = bufnr, remap = false }
-    vim.keymap.set("n", "<leader>f", function()
+  if client.supports_method("textDocument/formatting") then
+    local allow_format = disallow_format({ "tsserver", "eslint" })
+    vim.keymap.set("n", "<C-f>", function()
+      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), filter = allow_format, timeout_ms = 10000 })
+    end, { buffer = bufnr, desc = "[lsp] format" })
+    -- format on save
+    vim.api.nvim_clear_autocmds({ buffer = bufnr, group = group })
+    vim.api.nvim_create_autocmd(event, {
+      buffer = bufnr,
+      group = group,
+      callback = function()
         vim.lsp.buf.format({
-            async = false,
-            timeout_ms = 10000,
-            filter = disallow_format({ "tsserver", "eslint" }),
+          bufnr = bufnr,
+          async = async,
+          filter = allow_format,
+          timeout_ms = 10000
         })
-    end, opts)
+      end,
+      desc = "[lsp] format on save",
+    })
+  end
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  if client.supports_method("textDocument/rangeFormatting") then
+    vim.keymap.set("x", "<C-f>", function()
+      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf(), filter = allow_format, timeout_ms = 10000 })
+    end, { buffer = bufnr, desc = "[lsp] format" })
+  end
+
+  local opts = { buffer = bufnr, remap = false }
+  vim.keymap.set("n", "<leader>f", function()
+    vim.lsp.buf.format({
+      async = false,
+      timeout_ms = 10000,
+      filter = disallow_format({ "tsserver", "eslint" }),
+    })
+  end, opts)
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 lsp.setup();
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
--- lua/exsjabe/packer.lua
+++ lua/exsjabe/packer.lua
@@ -1,80 +1,84 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 -- Only required if you have packer configured as `opt`
 if (not vim.g.vscode) then
-    vim.cmd [[packadd packer.nvim]]
+  vim.cmd [[packadd packer.nvim]]
 
-    return require('packer').startup(function()
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
+  return require('packer').startup(function()
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-        use {
-            'nvim-telescope/telescope.nvim', tag = '0.1.1',
-            -- or                            , branch = '0.1.x',
-            requires = { { 'nvim-lua/plenary.nvim' } }
-        }
+    use {
+      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      -- or                            , branch = '0.1.x',
+      requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-        use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-        use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-        use 'ThePrimeagen/harpoon'
-        use 'ThePrimeagen/refactoring.nvim'
-        use 'mbbill/undotree'
-        use 'tpope/vim-fugitive'
-        use("nvim-treesitter/nvim-treesitter-context");
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use 'ThePrimeagen/harpoon'
+    use 'ThePrimeagen/refactoring.nvim'
+    use 'mbbill/undotree'
+    use 'tpope/vim-fugitive'
+    use("nvim-treesitter/nvim-treesitter-context");
 
-        use {
-            'VonHeikemen/lsp-zero.nvim',
-            branch = 'v2.x',
-            requires = {
-                -- LSP Support
-                { 'neovim/nvim-lspconfig' }, -- Required
-                {
-                    -- Optional
-                    'williamboman/mason.nvim',
-                    run = function()
-                        pcall(vim.cmd, 'MasonUpdate')
-                    end,
-                },
-                { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    use {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v2.x',
+      requires = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' }, -- Required
+        {
+          -- Optional
+          'williamboman/mason.nvim',
+          run = function()
+            pcall(vim.cmd, 'MasonUpdate')
+          end,
+        },
+        { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-                -- Autocompletion
-                { 'hrsh7th/nvim-cmp' },     -- Required
-                { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-                { 'L3MON4D3/LuaSnip' },     -- Required
-            }
-        }
-        use("folke/zen-mode.nvim")
-        use 'nvim-tree/nvim-web-devicons'
-        use {
-            "phha/zenburn.nvim",
-            config = function() require("zenburn").setup() end
-        }
-        use {
-            "zbirenbaum/copilot.lua",
-            cmd = "Copilot",
-            event = "InsertEnter",
-            config = function()
-                require("copilot").setup({
-                    suggestion = { enabled = false },
-                    panel = { enabled = false },
-                })
-            end,
-        }
-        use 'folke/trouble.nvim'
-        use("eandrju/cellular-automaton.nvim")
-        use("laytan/cloak.nvim")
-        use { 'jose-elias-alvarez/null-ls.nvim' }
-        use 'NLKNguyen/papercolor-theme'
-        use "savq/melange-nvim"
-        use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim', commit = "8c17024" }
-        use {
-            "zbirenbaum/copilot-cmp",
-            after = { "copilot.lua" },
-            config = function()
-                require("copilot_cmp").setup()
-            end
-        }
-        use { 'fgheng/winbar.nvim' }
-        use { 'styled-components/vim-styled-components' }
-    end)
+        -- Autocompletion
+        { 'hrsh7th/nvim-cmp' },     -- Required
+        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+        { 'L3MON4D3/LuaSnip' },     -- Required
+      }
+    }
+    use("folke/zen-mode.nvim")
+    use 'nvim-tree/nvim-web-devicons'
+    use {
+      "phha/zenburn.nvim",
+      config = function() require("zenburn").setup() end
+    }
+    use {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      config = function()
+        require("copilot").setup({
+          suggestion = { enabled = false },
+          panel = { enabled = false },
+          filetypes = {
+            markdown = true,
+            yaml = true
+          }
+        })
+      end,
+    }
+    use 'folke/trouble.nvim'
+    use("eandrju/cellular-automaton.nvim")
+    use("laytan/cloak.nvim")
+    use { 'jose-elias-alvarez/null-ls.nvim' }
+    use 'NLKNguyen/papercolor-theme'
+    use "savq/melange-nvim"
+    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim', commit = "8c17024" }
+    use {
+      "zbirenbaum/copilot-cmp",
+      after = { "copilot.lua" },
+      config = function()
+        require("copilot_cmp").setup()
+      end
+    }
+    use { 'fgheng/winbar.nvim' }
+    use { 'styled-components/vim-styled-components' }
+  end)
 end
