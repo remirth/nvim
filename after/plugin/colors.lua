954df8633fec42f70340e166dbd4400ff9d3c4f9
Exsjabe <jakob.bergstrom@explizit.se>
2023-12-12 10:09:44 +0100

    rosepine

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,7 +1,7 @@
 if (not vim.g.vscode) then
   function ColorMyPencils(color)
     -- vim.cmd.set("background=dark")
-    color = color or "catppuccin-mocha"
+    color = color or "rose-pine"
     vim.cmd.colorscheme(color)
 
     -- vim.api.nvim_set_hl(0, "Normal", { bg = "white" })
