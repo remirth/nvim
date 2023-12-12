ba62ffaa342947e0d35494fc53a0e04c00467e68
Exsjabe <jakob.bergstrom@explizit.se>
2023-12-12 15:24:17 +0100

    undo auto toggle

--- after/plugin/zenmode.lua
+++ after/plugin/zenmode.lua
@@ -1,42 +1,31 @@
 if (not vim.g.vscode) then
-  require("zen-mode").setup {
-    window = {
-      width = 80,
-      options = { }
-    },
-  }
-  require("zen-mode").toggle()
-  vim.wo.wrap = false
-  vim.wo.number = true
-  vim.wo.rnu = true
-  ColorMyPencils()
-  vim.keymap.set("n", "<leader>zz", function()
-    require("zen-mode").setup {
-      window = {
-        width = 80,
-        options = { }
-      },
-    }
-    require("zen-mode").toggle()
-    vim.wo.wrap = false
-    vim.wo.number = true
-    vim.wo.rnu = true
-    ColorMyPencils()
-  end)
+    vim.keymap.set("n", "<leader>zz", function()
+        require("zen-mode").setup {
+            window = {
+                width = 80,
+                options = { }
+            },
+        }
+        require("zen-mode").toggle()
+        vim.wo.wrap = false
+        vim.wo.number = true
+        vim.wo.rnu = true
+        ColorMyPencils()
+    end)
 
 
-  vim.keymap.set("n", "<leader>zZ", function()
-    require("zen-mode").setup {
-      window = {
-        width = 80,
-        options = { }
-      },
-    }
-    require("zen-mode").toggle()
-    vim.wo.wrap = false
-    vim.wo.number = false
-    vim.wo.rnu = false
-    vim.opt.colorcolumn = "0"
-    ColorMyPencils()
-  end)
+    vim.keymap.set("n", "<leader>zZ", function()
+        require("zen-mode").setup {
+            window = {
+                width = 80,
+                options = { }
+            },
+        }
+        require("zen-mode").toggle()
+        vim.wo.wrap = false
+        vim.wo.number = false
+        vim.wo.rnu = false
+        vim.opt.colorcolumn = "0"
+        ColorMyPencils()
+    end)
 end
