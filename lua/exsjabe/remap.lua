0a4d30388d5747b72ca8228a2f3d9799c77854c2
Exsjabe <jakob.bergstrom@explizit.se>
2024-01-15 16:47:49 +0100

    added remap

--- lua/exsjabe/remap.lua
+++ lua/exsjabe/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("n", "<C-q>", "<C-6>")
 vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>tf", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>tf", ":e .<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
