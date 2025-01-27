9feba08b58d6d7f2dc2db7fbf480ece16333d856
exsjabe <jakob.bergstrom@explizit.se>
2025-01-27 13:22:16 +0100

    add lua snip

--- lua/exsjabe/packer.lua
+++ lua/exsjabe/packer.lua
@@ -16,6 +16,14 @@ if (not vim.g.vscode) then
 
 
     use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use({
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!:).
+      run = "make install_jsregexp"
+    })
+
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use {
